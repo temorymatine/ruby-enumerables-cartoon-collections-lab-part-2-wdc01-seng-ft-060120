fe027f3093de6d10c25bfdd6354e51721d6a3c5a
Matine temory <temorymatine@gmail.com>
2020-05-25 19:02:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,7 +33,7 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
 
- planeteer_calls.filter_map{
+ planeteer_calls.filter_map{|i| 
  
  
  end
