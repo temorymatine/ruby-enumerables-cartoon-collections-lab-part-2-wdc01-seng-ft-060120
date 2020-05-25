ce8cca7cfa4db35346e76031cb2a8b951fe48371
Matine temory <temorymatine@gmail.com>
2020-05-25 19:24:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,7 +33,7 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
 
- puts planeteer_calls.any?{|a|  a = valid_calls.map{|n|n=n }}.first
+  planeteer_calls.find{|a|  a = valid_calls.first}
 
  
  
